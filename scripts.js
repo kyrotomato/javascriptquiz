0a662808b6acc81a78f2464d1cc6819b049c7480
kyrotomato <97704480+kyrotomato@users.noreply.github.com>
2022-03-03 18:58:04 -0600

    got questions to change with next click, applied initial choices values

--- scripts.js
+++ scripts.js
@@ -19,24 +19,24 @@ var questions = [{
     question: "The external javascript file must contain the <script> tag",
     options: ["True", "False"],
     correctAnswer: 2
-},{
-	question: "JavaScript is the same as Java,",
+}, {
+    question: "JavaScript is the same as Java,",
     options: ["True", "False"],
     correctAnswer: 2
-},{
-	question: "how do you declare a JavaScript variable with the name example?",
+}, {
+    question: "how do you declare a JavaScript variable with the name example?",
     options: ["Var example;", "var-example;", "var = example;", "var = ('example')"],
     correctAnswer: 3
-},{
-	question: "Is Javascript case sensitive",
+}, {
+    question: "Is Javascript case sensitive",
     options: ["Yes", "No"],
     correctAnswer: 3
-},{
-	question: "Which operator is used to assign a value to a variable?",
+}, {
+    question: "Which operator is used to assign a value to a variable?",
     options: ["'X'", "'='", "'-'", "no operator is used"],
     correctAnswer: 3
-},{
-	question: "How do you find the number with the highest value of x and y",
+}, {
+    question: "How do you find the number with the highest value of x and y",
     options: ["Math.ceil(x, y)", "Ceil(x, y)", "Math.max(x, y)", "Math.High(x, y)"],
     correctAnswer: 3
 }];
@@ -51,51 +51,72 @@ quiz = document.querySelector("#quiz");
 
 button = document.getElementById("start");
 button.addEventListener("click", startQuiz);
-function startQuiz(){
+function startQuiz() {
     //create container to hold everything
     var quizContainerEl = document.createElement("div");
     //append quizContainerEl to the quiz div
     quiz.appendChild(quizContainerEl);
-    
+
     //create div for the question
     var questionContainerEl = document.createElement("div");
+    //give the div an id
+    questionContainerEl.setAttribute("id", "questioncontainer");
     quizContainerEl.appendChild(questionContainerEl);
+    //change container text to question from array
     questionContainerEl.innerText = questions[qCount].question;
     //create el for the choices
     var choicesContainerEl = document.createElement("ul");
     questionContainerEl.appendChild(choicesContainerEl);
-        //four spaces for answers
-        //1
-        var choice = document.createElement("ol");
-        choicesContainerEl.appendChild(choice);
-        //2
-        var choice2 = document.createElement("ol");
-        choicesContainerEl.appendChild(choice2);
-        //3
-        var choice3 = document.createElement("ol");
-        choicesContainerEl.appendChild(choice3);
-        //4
-        var choice4 = document.createElement("ol");
-        choicesContainerEl.appendChild(choice4);
+    //four spaces for answers
+    //1
+    var choice = document.createElement("ol");
+    choicesContainerEl.appendChild(choice);
+    choice.innerText = questions[qCount].options[0]
+    //2
+    var choice2 = document.createElement("ol");
+    choicesContainerEl.appendChild(choice2);
+    choice2.innerText = questions[qCount].options[1]
+    //3
+    var choice3 = document.createElement("ol");
+    choicesContainerEl.appendChild(choice3);
+    choice3.innerText = questions[qCount].options[2]
+    //4
+    var choice4 = document.createElement("ol");
+    choicesContainerEl.appendChild(choice4);
+    choice4.innerText = questions[qCount].options[3]
     //button container
-        var buttonBox = document.createElement("div");
-        quizContainerEl.appendChild(buttonBox);
-        //create previous button
-        var buttonPrev = document.createElement("button");
-        buttonPrev.innerHTML = "Previous";
-        buttonBox.appendChild(buttonPrev);
-        //create next button
-        var buttonNext = document.createElement("button");
-        buttonNext.innerHTML = "Next";
-        buttonBox.appendChild(buttonNext);
-        start.remove();
-        buttonNext.addEventListener("click", nextQ);
+    var buttonBox = document.createElement("div");
+    quizContainerEl.appendChild(buttonBox);
+    //create previous button
+    var buttonPrev = document.createElement("button");
+    buttonPrev.innerHTML = "Previous";
+    buttonBox.appendChild(buttonPrev);
+    //create next button
+    var buttonNext = document.createElement("button");
+    buttonNext.innerHTML = "Next";
+    buttonBox.appendChild(buttonNext);
+    start.remove();
+    buttonNext.addEventListener("click", nextQ);
+    
 }
 
 
 
-function nextQ(){
-    
+function nextQ() {
+    //increase the q count so the question increase
+    qCount = qCount + 1;
+    //target the created div id
+    document.getElementById(questioncontainer);
+    //set the text of the container to the next question
+    questioncontainer.innerText = questions[qCount].question;
+
+    //target the choice elements
+        //1
+        //innertext to 
+        //2
+        //3
+        //4
+
 };
 //var count = 0
 //count i++1
